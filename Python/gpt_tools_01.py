e6a58dc3f2a57f2a69d6209d1ef83899abbf34a2
kvoloshenko <kvoloshenko@gmail.com>
2024-01-25 20:51:03 +0300

    Initial commit

--- Python/gpt_tools_01.py
+++ Python/gpt_tools_01.py
@@ -26,21 +26,21 @@ def get_simple_answer_gpt(system_content, user_content, model, temperature, clie
     )
     return completion  # возвращает ответ
 
-# def get_transcript(client, file_name, language="en"):
-#   logger.debug('get_transcript')
-#   logger.debug(type(language), f'language={language}')
-#   # https://platform.openai.com/docs/guides/speech-to-text/transcriptions
-#
-#
-#   audio_file= open(file_name, "rb")
-#   transcript = client.audio.transcriptions.create(
-#     model="whisper-1",
-#     language=language,
-#     file=audio_file,
-#     response_format="text"
-#   )
-#   logger.debug(type(transcript),'\n',transcript)
-#   return transcript
+def get_transcript(client, file_name, language="en"):
+  logger.debug('get_transcript')
+  logger.debug(type(language), f'language={language}')
+  # https://platform.openai.com/docs/guides/speech-to-text/transcriptions
+
+
+  audio_file= open(file_name, "rb")
+  transcript = client.audio.transcriptions.create(
+    model="whisper-1",
+    language=language,
+    file=audio_file,
+    response_format="text"
+  )
+  logger.debug(type(transcript),'\n',transcript)
+  return transcript
 
 def get_answer_gpt(system_content, user_content, ba, model, temperature, number_relevant_chunks, db_dir_name, client):
   embeddings = OpenAIEmbeddings()
